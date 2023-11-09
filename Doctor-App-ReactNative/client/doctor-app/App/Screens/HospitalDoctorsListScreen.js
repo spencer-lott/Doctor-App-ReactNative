9e428f984ad2982622e08f5aa290176abab58210
Spencer Lott <spklott@gmail.com>
2023-11-09 09:54:50 -0500

    created custom tabs

--- Doctor-App-ReactNative/client/doctor-app/App/Screens/HospitalDoctorsListScreen.js
+++ Doctor-App-ReactNative/client/doctor-app/App/Screens/HospitalDoctorsListScreen.js
@@ -2,6 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { useRoute } from '@react-navigation/native'
 import PageHeader from '../Components/Shared/PageHeader';
+import HospitalDoctorTab from '../Components/HospitalDoctorsScreen/HospitalDoctorTab';
 
 export default function HospitalDoctorsListScreen() {
 
@@ -10,6 +11,7 @@ export default function HospitalDoctorsListScreen() {
   return (
     <View style={{padding: 20}}>
       <PageHeader title={param?.categoryName}/>
+      <HospitalDoctorTab />
     </View>
   )
 }
